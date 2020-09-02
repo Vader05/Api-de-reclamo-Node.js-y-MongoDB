3f40362de91f4262bc9c0e6e37db01e5ebc4085a
Joseff361 <joseff361@outlook.com>
2020-09-02 13:03:53 -0500

    Get y Post implementados

--- routes/servicesRouter.js
+++ routes/servicesRouter.js
@@ -7,12 +7,25 @@ const serviceRouter = express.Router();
 serviceRouter.route('/')
 .get((req, res, next) => {
     Services.find({})
-    .then((promotions) => {
+    .then((services) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
-        res.json(promotions) 
+        res.json(services) 
     }, (err) => next(err)) 
     .catch((err) => next(err))
 })
-
+.post((req, res, next) => {
+    Services.create(req.body)
+    .then((services) => {
+        console.log('Service Created ', services)
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(services)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.put((req, res, next) => {
+    res.statusCode = 403
+    res.end('PUT operation not supported on /services')
+})
 module.exports = serviceRouter;
