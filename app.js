b7d16a537102f77471f3e95f9c8e7acc1af32394
Joseff361 <joseff361@outlook.com>
2020-09-02 11:37:18 -0500

    Ordenando configuracion

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//3party modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose =  require('mongoose');
+var config = require('../Api-de-reclamo-Node.js-y-MongoDB/config/config');
 
+
+//Importing routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var employeeRouter= require('./routes/api/employee'); 
@@ -12,26 +16,36 @@ var employeeRouter= require('./routes/api/employee');
 var app = express();
 
 //conection to mongoDB
-var mongoBD = 'mongodb://localhost/backendRest';
-mongoose.connect(mongoBD, {useNewUrlParser: true});
-mongoose.Promise= global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, "MongoBD conection error"));
+const connect = mongoose.connect(config.mongoUrl, { 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true,
+  useFindAndModify: false  
+});
+
+connect.then(cb => {
+  console.log('Correctly connected to MongoDB');
+}).catch(err => console.log(err));
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+//Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/employee', employeeRouter);
+
+
+
+
+
+//ErrorHandling
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
