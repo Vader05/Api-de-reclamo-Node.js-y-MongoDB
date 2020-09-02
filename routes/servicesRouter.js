65d3ec8385e1da5794dbd26a45951aaf38da126b
Joseff361 <joseff361@outlook.com>
2020-09-02 13:08:12 -0500

    ServicesRouter terminado

--- routes/servicesRouter.js
+++ routes/servicesRouter.js
@@ -37,4 +37,42 @@ serviceRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
+
+
+promotionRouter.route('/:serviceId')
+.get((req, res, next) => {
+    Services.findById(req.params.promoId)
+    .then((service) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(service)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.post((req, res, next) => {
+    res.statusCode = 403 
+    res.end(`POST operation not supported on /services/${req.params.serviceId}`)
+})
+.put((req, res, next) => {
+    Services.findByIdAndUpdate(req.params.serviceId, {
+        $set: req.body
+    }, { new: true }) 
+    .then((service) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(service)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.delete((req, res, next) => {
+    Services.findByIdAndRemove(req.params.serviceId)
+    .then((resp) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(resp)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+
+
 module.exports = serviceRouter;
