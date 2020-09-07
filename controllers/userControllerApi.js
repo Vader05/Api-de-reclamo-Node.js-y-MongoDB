978d89224b4ca3f3d3c810324d167d7e1465bb98
Joseff361 <joseff361@outlook.com>
2020-09-06 23:45:47 -0500

    2

--- controllers/userControllerApi.js
+++ controllers/userControllerApi.js
@@ -3,15 +3,13 @@ var User = require('../models/user');
 exports.userLIst = function(req, res){
     User.find({}, function(err, user){
         if(err) console.log(err);
-        res.status(200).json({
-            users: user
-        });
+        res.status(200).json(user);
     });
 }
 
 
 exports.user_create = function(req, res){
-    var user= new User({username: req.body.firstname, password: req.body.lastname, employee: req.body.admin});
+    var user= new User({username: req.body.username, password: req.body.password, employee: req.body.employee});
     user.save(function(err, result){
         if(err) return res.status(500).json(err);
         res.status(200).json(result); 
