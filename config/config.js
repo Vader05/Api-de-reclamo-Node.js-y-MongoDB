38f0a8b1ae971a249569989f9453838cb5bfdd5e
Kevin Cueva <red1rc2@gmail.com>
2020-09-27 21:11:33 -0500

    todo listo

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //'mongoUrl': 'mongodb://localhost/backendRest',
-    'mongoUrl': 'mongodb+srv://Lordvader:Darthvader05@cluster0.vucmz.mongodb.net/backendRest?retryWrites=true&w=majority'
+    'mongoUrl': 'mongodb://localhost/backendRest',
+    
 }
